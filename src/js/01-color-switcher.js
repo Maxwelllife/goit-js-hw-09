8eebe457aecb8807206901594473b85d8755c7f9
Maksym Pokulytyi <mlightpro@gmail.com>
2022-07-08 12:22:49 +0300

    add resolved task 9.1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+refs = {
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector('[data-stop]'),
+  bodyStyle: document.body.style,
+};
+refs.start.addEventListener('click', onStartBtnClick);
+refs.stop.addEventListener('click', onStopBtnClick);
 
+refs.stop.disabled = true;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onStartBtnClick() {
+  timeoutId = setInterval(() => {
+    refs.bodyStyle.backgroundColor = getRandomHexColor();
+  }, 1000);
+  refs.start.disabled = true;
+  refs.stop.disabled = false;
+}
+
+function onStopBtnClick() {
+  clearInterval(timeoutId);
+  refs.start.disabled = false;
+  refs.stop.disabled = true;
+}
