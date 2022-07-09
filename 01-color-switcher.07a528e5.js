f5b11fe4f25af2e954a6464b91b20f7fb163f8e2
Maxwelllife <37122707+Maxwelllife@users.noreply.github.com>
2022-07-09 09:01:11 +0000

    Deploying to gh-pages from @ Maxwelllife/goit-js-hw-09@692337c6cdcc931ab81d3db596afd522d38d7ee4 🚀

---
+++ 01-color-switcher.07a528e5.js
@@ -0,0 +1,2 @@
+const t={start:document.querySelector("[data-start]"),stop:document.querySelector("[data-stop]"),bodyStyle:document.body.style};t.start.addEventListener("click",(function(){timeoutId=setInterval((()=>{t.bodyStyle.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.start.disabled=!0,t.stop.disabled=!1})),t.stop.addEventListener("click",(function(){clearInterval(timeoutId),t.start.disabled=!1,t.stop.disabled=!0})),t.stop.disabled=!0;
+//# sourceMappingURL=01-color-switcher.07a528e5.js.map
